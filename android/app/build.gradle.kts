4ebf3f4cf18a394c49cf06f03ff7e9d0d38c9f4f
Abdul Qoyyuum <aqoyyuum@cardaccess.com.au>
2025-10-18 11:26:15 +0800

    refactor: move import statements to top of build.gradle.kts file

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
-
-import java.util.Properties
-import java.io.FileInputStream
 
 // Load keystore properties
 val keystorePropertiesFile = rootProject.file("key.properties")
